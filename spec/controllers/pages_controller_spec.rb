5c50267cd4a603bbe874306e47d56b8c4f51fe1c
Will Farrington <will@railsmachine.com>
2011-05-28 17:40:32 -0400

    Include redirection testing.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,7 @@ describe PagesController do
 
   describe 'no user signed in' do
     it "should redirect" do
-      get :home
+      get(:home).should redirect_to('/users/sign_in')
       response.status.should == 302
     end
   end
