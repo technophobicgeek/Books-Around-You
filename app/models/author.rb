8f4b89abedd21eee98d8a3e71723e7a327afaf28
Raj Bandyopadhyay <technophobicgeek@gmail.com>
2011-05-31 22:30:46 -0400

    Annotations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+# Schema version: 20110601022230
+#
+# Table name: authors
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  twitter    :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Author < ActiveRecord::Base
 end
