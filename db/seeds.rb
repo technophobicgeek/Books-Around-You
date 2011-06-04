c9c4ac16a40d9808419602584f48d54cc9e70e77
Raj Bandyopadhyay <technophobicgeek@gmail.com>
2011-06-04 11:32:09 -0400

    [#13935073] Adding seed data using rand

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# Create some users
+
+@users =
+  (0..4).map do |i|
+    User.create!(
+      :name => "User #{i}",
+      :nickname => "user#{i}",
+      :email => "user#{i}@example.com",
+      :password => "sekret#{i}",
+      :password_confirmation => "sekret#{i}"
+    )
+  end
+
+# Create some books
+
+@books = 
+  (0..49).map do |i|
+    Book.create!(
+      :isbn10 => "ISBN 10 #{i}",
+      :isbn13 => "ISBN 13 #{i}",
+      :title => "Book #{i}"
+    )
+  end
+
+
+# Create some copies
+
+@copies =
+  (0..99).map do |i|
+    u_num = rand(4)
+    b_num = rand(50)
+    Copy.create!(
+      :user => @users[u_num],
+      :book => @books[b_num],
+      :condition => "Good",
+      :description => "Copy #{i} of book #{b_num} belonging to user #{u_num}"
+    )     
+  end
+
